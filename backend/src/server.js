8e6858a61fa273acd913e066c754443d382e0e81
Ramragul <iotprograms@gmail.com>
2026-04-22 16:53:21 +0530

    First Phase release Changes backend build fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,11 @@ import app from "./app.js";
 
 const PORT = 3004;
 
+app.use((req, res, next) => {
+  console.log("🔥 REQUEST:", req.method, req.url);
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });

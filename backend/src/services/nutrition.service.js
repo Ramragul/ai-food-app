604ab38bb00fc2dbb9ab2226c683b7eeaa1e2193
Ramragul <iotprograms@gmail.com>
2026-06-07 14:40:36 +0530

    Add Meal Logic Improvements

--- backend/src/services/nutrition.service.js
+++ backend/src/services/nutrition.service.js
@@ -599,28 +599,28 @@ const grams =
 
  */
 
-const calculateNutritionFromGrams = (
-  food,
-  grams
-) => {
-  return {
-    calories:
-      (food.calories_per_100g / 100) *
-      grams,
+// const calculateNutritionFromGrams = (
+//   food,
+//   grams
+// ) => {
+//   return {
+//     calories:
+//       (food.calories_per_100g / 100) *
+//       grams,
 
-    protein:
-      (food.protein_per_100g / 100) *
-      grams,
+//     protein:
+//       (food.protein_per_100g / 100) *
+//       grams,
 
-    carbs:
-      (food.carbs_per_100g / 100) *
-      grams,
+//     carbs:
+//       (food.carbs_per_100g / 100) *
+//       grams,
 
-    fats:
-      (food.fats_per_100g / 100) *
-      grams,
-  };
-};
+//     fats:
+//       (food.fats_per_100g / 100) *
+//       grams,
+//   };
+// };
 
 /**
  * 🔥 STEP 2: SAVE TO DB (TRANSACTION SAFE)

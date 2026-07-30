e16c205662fc9bcc449483228b7770d25d4794f7
Ramragul <iotprograms@gmail.com>
2026-07-30 17:43:46 +0530

    Shipping My Coach Notes api from coach folder to newly created client folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ import fitnessRoutes from "./routes/fitness.routes.js";
 import organizationRoutes from "./routes/organization.routes.js";
 import coachRoutes from "./routes/coach.routes.js";
 import consentRoutes from "./routes/consent.routes.js";
+import clientRoutes from "./routes/client.routes.js";
 
 
 
@@ -39,6 +40,7 @@ app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use("/api/user", userRoutes);
+app.use("/api/client", clientRoutes);
 
 app.use("/api/meal-plan", mealPlanRoutes);
 

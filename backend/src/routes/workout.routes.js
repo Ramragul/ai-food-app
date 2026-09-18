43fdc2a9b9758c0a8133dab70bf1aebfa14fecac
Ramragul <iotprograms@gmail.com>
2026-09-18 17:56:39 +0530

    Workout Functionalities backend api implementations bug fix.

--- backend/src/routes/workout.routes.js
+++ backend/src/routes/workout.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 import {
   getMuscleGroups,
